842829f87bc0ff15ad23af87eb04499b96e597d0
LPSalazar <35040690+LPSalazar@users.noreply.github.com>
2018-02-07 11:32:13 -0300

    ejemplo Error

--- scripts/ejecutable.sh
+++ scripts/ejecutable.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-
-export MAVEN_HOME=/opt/apache-maven-3.5.2
-
-export M2_HOME=/opt/apache-maven-3.5.2
-export PATH=$PATH:$M2_HOME/bin 
-
-cd /home/ec2-user/pruebas/prueba2
+cd /home/ec2-user/Error/pruebaError
 mvn package
